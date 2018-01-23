06824a133c04f7992ddfe0266757fa264586bb0d
kogulan93 <kogulan.rajendran@hotmail.fr>
2018-01-23 11:47:35 +0100

    commit mardi matin 12

--- src/app/list-client/list-client.component.ts
+++ src/app/list-client/list-client.component.ts
@@ -1,15 +1,30 @@
+import { Client } from '../model/client';
+import { ServiceClient } from '../servicej/serviceclient';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-client',
   templateUrl: './list-client.component.html',
-  styleUrls: ['./list-client.component.css']
+  styleUrls: ['./list-client.component.css'],
+ providers: [ServiceClient]
 })
+
 export class ListClientComponent implements OnInit {
 
-  constructor() { }
+clients: Client[];
+
+
+  constructor(private service: ServiceClient) {
+
+  }
+
+getClient() {
+    this.service.getClients().subscribe(data => this.clients = data,
+      error => console.log('error inservice'));
+  }
 
   ngOnInit() {
+    this.getClient();
   }
 
 }
--- src/app/servicej/serviceclient.ts
+++ src/app/servicej/serviceclient.ts
@@ -1,3 +1,19 @@
-export class ServiceCLient {
-  
+import { Client } from '../model/client';
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+
+
+
+@Injectable()
+export class ServiceClient {
+
+   constructor(private http: Http) { }
+
+     getClients(): Observable<any> {
+    return this.http.get('http://localhost:8080/SimpleMavenJPAWeb/clients')
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw('Big issue!'));
+  }
+
 }
